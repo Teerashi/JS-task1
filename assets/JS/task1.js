e03e9e85e13e95dfa95afb710bec93654e5335ce
Teerashi <vito.trojan@gmail.com>
2023-11-21 15:37:54 +0200

    first commit

---
+++ assets/JS/task1.js
@@ -0,0 +1,33 @@
+let numb1 = 10;
+let numb2 = 10;
+
+const result1 = (numb1 * numb2);
+console.log (result1);
+
+const result2 = (result1 - 50);
+console.log (result2);
+
+const result3 = (result2 * result1);
+console.log (result3);
+
+const result4_1 = (result3 / 2.687);
+console.log (result4_1);
+const result4_2 = (result3 % 2.687);
+console.log (result4_2);
+
+const result5 = !!result1
+console.log (result5);
+
+let num = 5;
+
+num += 10;
+console.log(num);
+
+num *= 2;
+console.log (num);
+
+num -= 4;
+console.log (num);
+
+num /=6; 
+console.log (num);
